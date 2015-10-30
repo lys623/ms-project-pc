7aec40f7c1b2dabffd3713eb8b7ba7855115772e
lys623 <lys623@qq.com>
2015-10-30 19:05:36 +0800

    修改 配置文件

--- fis-conf.js
+++ fis-conf.js
@@ -1,42 +1,43 @@
+fis.hook('relative');
+fis.match('**', {
+    relative: true
+});
+
+
 fis.match('::packager', {
-  postpackager: fis.plugin('loader', {
-    //allInOne: true
-  })
+    postpackager: fis.plugin('loader', {
+        allInOne:{
+            css: 'css/index.css',
+            js: 'js/index.js',
+        }
+    })
 })
 
 fis.match('*.html', {
-  postprocessor:fis.plugin('format')
+    postprocessor:fis.plugin('format')
 })
 
 // 启用 fis-spriter-csssprites 插件
 fis.match('::package', {
-  spriter: fis.plugin('csssprites')
+    spriter: fis.plugin('csssprites')
+})
+fis.match('**/(*.png)', {
+    release:'css/i/$1',
+})
+
+fis.match('**/(*.jpg)', {
+    release:'css/i/$1'
 })
 // 对 CSS 进行图片合并
 fis.match('*.{css,less}', {
-  // 给匹配到的文件分配属性 `useSprite`
-  useSprite: true,
-  parser: fis.plugin('cssnext')
+    // 给匹配到的文件分配属性 `useSprite`
+    useSprite: true,
+    parser: fis.plugin('cssnext')
 });
-
-
-fis.media('prod')
-    .match('::package', {
-      postpackager: fis.plugin('loader', {
-        //allInOne: {
-        //  css: 'pkg/web_app_${hash}.css',
-        //  js: 'pkg/web_app_${hash}.js'
-        //}
-      })
+fis.match('*.less', {
+    rExt: '.css',
+    parser: fis.plugin('less-2.x', {
     })
-    .match('*.js', {
-      useHash: true,
-      optimizer: fis.plugin('uglify-js')
-    })
-    .match('*.css', {
-      useHash: true,
-      optimizer: fis.plugin('clean-css')
-    })
-    .match('*.png', {
-      optimizer: fis.plugin('png-compressor')
-    })
+}).match('*.png', {
+    optimizer: fis.plugin('png-compressor')
+})
